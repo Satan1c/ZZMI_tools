5c227d7b2e077d686c315ca2f8c03aca0a35cd87
Olexander <satan1c@ukr.net>
2025-05-04 23:37:20 +0300

    slotfix applier

---
+++ SlotsFixApplier/Program.cs
@@ -0,0 +1,25 @@
+﻿using SlotsFixApplier;
+
+const string modFolder = @"C:\Users\Satan1c\OneDrive\Games\For games\mods\XXMI Launcher\ZZMI\Mods\Test\Pulchra";
+
+IAppliers[] appliers =
+[
+	new Slotted()
+];
+
+var applier = appliers[0];
+
+foreach (var ini in Directory.EnumerateFiles(modFolder, "*.ini", SearchOption.AllDirectories))
+{
+	var fileInfo = new FileInfo(ini);
+	var name = fileInfo.Name;
+	if (name.Trim().StartsWith("DISABLED")) continue;
+
+	var path = Path.GetDirectoryName(fileInfo.FullName) ?? string.Empty;
+
+	var resultPath = Path.Combine(path, "result.ini");
+	applier.Execute(ini, resultPath);
+
+	File.Move(ini, Path.Combine(path, "DISABLED SlotFixApplied " + name));
+	File.Move(resultPath, Path.Combine(path, name));
+}
