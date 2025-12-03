e2b27e05f5d01b26330a25ebb428a0aabbded954
oleksandr.fedotov <oleksandr.fedotov@cyberpolice.gov.ua>
2025-12-03 15:48:18 +0200

    update default logging mode to standard

--- VersionFixerGenerator/VersionFixer.cs
+++ VersionFixerGenerator/VersionFixer.cs
@@ -11,7 +11,7 @@ if (args[0] is "-h" or "--help")
 {
 	Console.WriteLine("Options:");
 	Console.WriteLine("  -l, --logging [v|s|n]   Change logging mode: v - verbose, s - standard, n - none");
-	Console.WriteLine("                          Default is verbose");
+	Console.WriteLine("                          Default is standard");
 	Console.WriteLine("  -p, --path \"[path]\"   Change the path to the Mods folder with PlayerCharacterData.json");
 	Console.WriteLine("                          Default is current directory");
 	Console.WriteLine("Commands:");
